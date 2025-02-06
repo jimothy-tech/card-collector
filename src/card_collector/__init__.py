0d01e7b427113823c31b02c49f57317c158e4cfb
jimothy <timothynewton1998@gmail.com>
2025-02-06 01:37:29 -0500

    create app factory

--- src/card_collector/__init__.py
+++ src/card_collector/__init__.py
@@ -1,2 +1,26 @@
-def hello() -> str:
-    return "Hello from card-collector!"
+from flask import Flask
+import mongoengine as me
+import os
+from flask_session import Session
+from card_collector.routes import api
+
+
+def create_app(config_class=None, logging_args=None):
+    app = Flask(__name__)
+
+    if config_class is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.from_object(config_class)
+
+    # Initialize MongoEngine
+    # me.connect(host=os.getenv("MONGO_URI_DEV"), db="Auth_Test")
+
+    # Register app with Flask-Session
+    Session(app)
+
+    app.register_blueprint(api)
+
+    # Register blueprints
+
+    return app
